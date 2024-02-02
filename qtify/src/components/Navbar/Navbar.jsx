9a2839705a0a0cef21a7e2a80bdde921bb2a8bd7
SidhantSwaraj <sidhantswaraj@gmail.com>
2024-02-02 20:14:13 +0530

    cardsection

--- qtify/src/components/Navbar/Navbar.jsx
+++ qtify/src/components/Navbar/Navbar.jsx
@@ -3,22 +3,22 @@ import Logo from "../Logo/Logo";
 import Search from "../Search/Search";
 import Button from "../Button/Button";
 import styles from "./Navbar.module.css";
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function Navbar({ searchData }) {
     return (
-        
+        <>
         <nav className={styles.navbar}>
-            <a href="/">
+            <Link to="/">
                 <Logo/>
-            </a>
+            </Link>
             <Search
                 placeholder="Search a song of your choice"
                 searchData={searchData}
             />
             <Button>Give Feedback</Button>
         </nav>
-        
+        </>
     )
 }
 
